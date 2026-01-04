4711d4b5dc93a8f6db4e1c87c500dfcc04ac6e66
mahmoudfathy <mahmoudfathy.dev@gmail.com>
2026-01-04 15:19:17 +0200

    Update project files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Portfolio/' : '/',
+  base: mode === 'production' ? '/Portfolio-Clean/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
