338892aec8c593707a8280cdda2f6c81b0fa940d
mahmoudfathy <mahmoudfathy.dev@gmail.com>
2026-01-04 10:16:30 +0200

    Fix Navbar.scss case

--- src/Components/Global/Navbar/Navbar.jsx
+++ src/Components/Global/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from '../../../images/logo.png'
 import { NavLink } from 'react-router-dom'
-import './navbar.scss'
+import './Navbar.scss'
 import Button from '../../UI/Button/Button'
 
 
